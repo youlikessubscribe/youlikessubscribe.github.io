0daed9dd0d1202f4bcd935a41ad76354e5c7d3d4
rishabhasthana06 <rishabhasthana06@gmail.com>
2017-11-10 22:05:54 +0530

    first website upload

---
+++ m.php
@@ -0,0 +1,370 @@
+<!DOCTYPE html>
+<html>
+<title>MySwaggs | Home</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open Sans">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+h1,h2,h3,h4,h5,h6 {font-family: "Oswald"}
+body {font-family: "Open Sans"}
+</style>
+<style>
+.dropdown {
+    position: relative;
+    display: inline-block;
+}
+
+.dropdown-content {
+    display: none;
+    position: absolute;
+    background-color: #f9f9f9;
+    min-width: 160px;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    padding: 12px 16px;
+    z-index: 1;
+}
+
+.dropdown:hover .dropdown-content {
+    display: block;
+}
+</style>
+<body class="w3-light-grey">
+<div class="w3-row">
+	<div class="w3-half">
+		<div class="w3-display-container">
+			<img src="image1.jpg">
+			<div class="w3-display-position" style="left: 45%;top: 28%;right:10%;bottom:15%;">
+              <div class="dropdown">
+                <div class="w3-border">&oplus;</div>
+                <div class="dropdown-content w3-card-4" style="width:200px">
+                  <div class="w3-row">
+                    <div class="w3-half">
+                      <img src="https://cdn.shopify.com/s/files/1/1970/7471/files/product-image-277153971_grande_26e53808-dc9b-49e9-956e-0e0d1a3548ed.jpg?9298043990276862993" width="100%">
+                    </div>
+                    <div class="w3-half">
+                      <p class="w3-tiny">Compression power leggings.</p>
+                      <div class="w3-display-container w3-padding"><p><button class="w3-right w3-button w3-red w3-tiny">BUY</button></p></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="w3-display-position" style="left: 25%;top: 8%;right:60%;bottom:15%;">
+              <div class="dropdown">
+                <div class="w3-border">&oplus;</div>
+                <div class="dropdown-content w3-card-4" style="width:200px">
+                  <div class="w3-row">
+                    <div class="w3-half">
+                      <img src="https://cdn.shopify.com/s/files/1/1970/7471/files/product-image-277153971_grande_26e53808-dc9b-49e9-956e-0e0d1a3548ed.jpg?9298043990276862993" width="100%">
+                    </div>
+                    <div class="w3-half">
+                      <p class="w3-tiny">Compression power leggings.</p>
+                      <div class="w3-display-container w3-padding"><p><button class="w3-right w3-button w3-red w3-tiny">BUY</button></p></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+		</div>
+
+		
+		</div>
+
+	</div>
+	<div class="w3-half">
+		<div class="w3-display-container">
+			<img src="image2.jpg">
+		</div>
+	</div>
+</div>
+</body>
+</html>
+
+
+
+<div class="w3-display-container">
+            <img src="https://cdn.shopify.com/s/files/1/1970/7471/files/product-image-306369115_grande_51785a62-48a5-4c86-9387-22a05c9e6166.jpg?2219415037387239399" width="100%">
+            <div class="w3-display-position" style="left: 45%;top: 28%;right:10%;bottom:15%;">
+                    <div class="dropdown">
+                      <div>&oplus;</div>
+                      <div class="dropdown-content w3-card-4" style="width:200px">
+                        <div class="w3-row">
+                          <div class="w3-half">
+                            <img src="https://cdn.shopify.com/s/files/1/1970/7471/files/product-image-277153971_grande_26e53808-dc9b-49e9-956e-0e0d1a3548ed.jpg?9298043990276862993" width="100%">
+                          </div>
+                          <div class="w3-half">
+                            <p class="w3-tiny">Compression power leggings.</p>
+                            <div class="w3-display-container w3-padding"><p><button class="w3-right w3-button w3-red w3-tiny">BUY</button></p></div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="w3-display-position" style="left: 48%;top: 48%;right:20%;bottom:15%;">
+                    <div class="dropdown">
+                      <div>&oplus;</div>
+                      <div class="dropdown-content w3-card-4" style="width:200px">
+                        <div class="w3-row">
+                          <div class="w3-half">
+                            <img src="https://cdn.shopify.com/s/files/1/1970/7471/files/product-image-277153971_grande_26e53808-dc9b-49e9-956e-0e0d1a3548ed.jpg?9298043990276862993" width="100%">
+                          </div>
+                          <div class="w3-half">
+                            <p class="w3-tiny">Your leggings.</p>
+                            <div class="w3-display-container w3-padding"><p><button class="w3-right w3-button w3-red w3-tiny">BUY</button></p></div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+          </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+          <header class="w3-display-container w3-wide" id="home"><a href="https://www.myswaggs.com/collections/fitness-wear" title="Activewear"><img src="https://cdn.shopify.com/s/files/1/1970/7471/files/big_image_grande_cdff68ef-2029-4a0a-8d5f-99e8261c8ee1.jpg?17232816934268540656" alt="Fashion Blog" width="100%" style="max-height: 750px;" /></a>
+<div class="w3-display-left w3-padding-large">
+<h1 class="w3-text-white w3-hide-small"><b><u>wh</u>y<u> we are obsessed</u></b></h1>
+<p class="w3-text-white w3-hide-large"><b><u>wh</u>y<u> we are obsessed</u></b></p>
+<br />
+<div class="w3-text-white w3-hide-small">
+<h3>Bum Sculpting</h3>
+<br />
+<h3>Ultra-lightweight</h3>
+<br />
+<h3>Sweat-wicking and quick drying</h3>
+<br />
+<h3>Sexy and comfortable</h3>
+</div>
+<div class="w3-text-white w3-hide-large w3-show-small">Bum Sculpting <br />Ultra-lightweight <br />Sweat-wicking and quick drying<br />Sexy and comfortable</div>
+</div>
+</header>
+<div class="w3-main w3-content w3-padding" style="max-width: 1200px; margin-top: 50px;">
+<div class="w3-row-padding">
+<center>
+<h3><strong>Ladies, We have news for you your wardrobe is calling out for these bum-sculpting leggings</strong>.<strong> After perfecting the fit and fabric for almost 5 years this is why we love them</strong>.</h3>
+<br /> <button class="w3-button w3-red">SHOP THE POWER LEGGINGS</button> <br />
+<hr />
+<br />
+<h2>GIVE YOUR BUM AN UPGRADE</h2>
+</center>
+<div class="w3-half">
+<div class="w3-row-padding">
+<h3>A pair for every wear</h3>
+<p><strong>Take your pic f</strong><strong>rom cropped or full length then select a range of beautiful prints and colors. We guarantee your workouts will be transformed.</strong></p>
+</div>
+<!-- FIRST IMAGE -->
+<div class="w3-row">
+<div class="w3-display-container"><img src="https://cdn.shopify.com/s/files/1/1970/7471/files/product-image-277153971_grande_26e53808-dc9b-49e9-956e-0e0d1a3548ed.jpg?9298043990276862993" width="100%" /> <!-- 1 + -->
+<div class="w3-display-position" style="left: 43%; top: 28%; right: 10%; bottom: 15%;">
+<div class="dropdown"><img src="https://cdn.shopify.com/s/files/1/1970/7471/files/blue_plus.png?17232816934268540656" width="50" height="50" />
+<div class="dropdown-content w3-card-4" style="width: 220px;">
+<div class="w3-row"><!-- FIRST image + -->
+<div class="w3-half"><img src="//cdn.shopify.com/s/files/1/1970/7471/files/product-image-210752696_grande_7f285e92-bb64-4b60-886a-fc66d7ecc0aa_large.jpg?v=1507232495" alt="" width="165" height="165" /></div>
+<div class="w3-half">
+<p style="font-size: 12px; padding-left: 5px;">Sexy and cute, wonderful sports bra available in many colors and varieties.</p>
+<div class="w3-display-container w3-padding">
+<p><button class="w3-right w3-button w3-red w3-tiny">BUY</button></p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<!-- 2 + -->
+<div class="w3-display-position" style="left: 50%; top: 48%; right: 20%; bottom: 15%;">
+<div class="dropdown"><img src="https://cdn.shopify.com/s/files/1/1970/7471/files/blue_plus.png?17232816934268540656" width="50" height="50" />
+<div class="dropdown-content w3-card-4" style="width: 250px;">
+<div class="w3-row">
+<div class="w3-half"><img src="//cdn.shopify.com/s/files/1/1970/7471/files/product-image-236786062_grande_3c27b33a-8557-478a-9cc2-e2892119d299_large.jpg?v=1507232382" alt="" width="163" height="163" /></div>
+<div class="w3-half">
+<p style="font-size: 12px; padding-left: 5px;">Must-have push-up leggings, Surely heading to the gym will be a fun moment!</p>
+<div class="w3-display-container w3-padding">
+<p><button class="w3-right w3-button w3-red w3-tiny">BUY</button></p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+ INFINITY WORKOUT BRA, ZERO GRAVITY RUN LEGGINGS.</div>
+<div class="w3-half"></div>
+<!-- END 1 image --> <!-- START 2 image -->
+<div class="w3-half">
+<div class="w3-row">
+<div class="w3-display-container"><img src="https://cdn.shopify.com/s/files/1/1970/7471/files/product-image-403417431_grande_a2f5d052-de38-46dc-9e97-86bb68831613.jpg?2219415037387239399" width="100%" style="max-height: 550px;" /> <!-- 3 + -->
+<div class="w3-display-position" style="left: 43%; top: 28%; right: 10%; bottom: 15%;">
+<div class="dropdown"><img src="https://cdn.shopify.com/s/files/1/1970/7471/files/blue_plus.png?13562271075209030075" width="50" height="50" />
+<div class="dropdown-content w3-card-4" style="width: 250px;">
+<div class="w3-row">
+<div class="w3-half"><img src="//cdn.shopify.com/s/files/1/1970/7471/files/Syprem-2017-New-Style-Women-Fitness-Bra-Sports-Yoga-Running-Sexy-Bra-High-Quality-Lady-Sportswear_2_grande_df1e4543-2b44-4783-95b5-f57eb1dd3743_large.jpg?v=1507231491" alt="" width="181" height="181" /></div>
+<div class="w3-half">
+<p style="font-size: 12px; padding-left: 5px;">Sexy and with a stunning design! these bras are one of the best sellers we have!...</p>
+<div class="w3-display-container w3-padding">
+<p><button class="w3-right w3-button w3-red w3-tiny">BUY</button></p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<!-- 4 + -->
+<div class="w3-display-position" style="left: 42%; top: 70%; right: 20%; bottom: 15%;">
+<div class="dropdown"><img src="https://cdn.shopify.com/s/files/1/1970/7471/files/blue_plus.png?13562271075209030075" width="50" height="50" />
+<div class="dropdown-content w3-card-4" style="width: 250px;">
+<div class="w3-row">
+<div class="w3-half"><img src="//cdn.shopify.com/s/files/1/1970/7471/files/3d-Push-Up-Print-Fitness-Workout-Sporting-Leggings-for-Women-Striped-Fitness-High-Waist-Slim-Trousers_grande_733ff542-3ef6-4872-9c66-9d170190f060_large.jpg?v=1507231896" alt="compression leggings" width="164" height="164" /></div>
+<div class="w3-half">
+<p style="font-size: 12px; padding-left: 5px;">Increase confidence in yourself wearing these white workout leggings(available in black)...</p>
+<div class="w3-display-container w3-padding">
+<p><button class="w3-right w3-button w3-red w3-tiny">BUY</button></p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+ ULTIMATE WORKOUT BRA, ZERO GRAVITY RUN LEGGINGS</div>
+</div>
+<br />
+<div class="w3-row-padding">
+<div class="w3-twothird">
+<div class="w3-display-container"><img src="https://cdn.shopify.com/s/files/1/1970/7471/files/product-image-174910804_grande_d616ff8c-9cf1-4d1d-9739-5cb0b973b301.png?2219415037387239399" width="80%" style="max-height: 545px;" /> <!-- 5 + -->
+<div class="w3-display-position" style="left: 45%; top: 28%; right: 10%; bottom: 15%;">
+<div class="dropdown"><img src="https://cdn.shopify.com/s/files/1/1970/7471/files/blue_plus.png?17232816934268540656" width="50" height="50" />
+<div class="dropdown-content w3-card-4" style="width: 250px;">
+<div class="w3-row">
+<div class="w3-half"><img src="//cdn.shopify.com/s/files/1/1970/7471/files/product-image-174910804_grande_54163969-85ef-41ad-808e-d10c731cbffb_large.png?v=1507231954" alt="" width="208" height="238" /></div>
+<div class="w3-half">
+<p style="font-size: 12px; padding-left: 5px;">Compression Tush Trainer leggings are the ultimate leggings to have for an efficient tush and bum sculpting. loved by many of MySwaggs babes!</p>
+<div class="w3-display-container w3-padding">
+<p><button class="w3-right w3-button w3-red w3-tiny">BUY</button></p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="w3-third">
+<div class="w3-row-padding">
+<h3>A pair for every wear</h3>
+<p><strong>Take your pic f</strong><strong>rom cropped or full length then select a range of beautiful prints and colors.</strong><strong> We guarantee</strong><strong> our workouts will be transformed.</strong></p>
+</div>
+<br />
+<div class="w3-row">
+<div class="w3-display-container"><img src="https://cdn.shopify.com/s/files/1/1970/7471/files/product-image-306369115_grande_51785a62-48a5-4c86-9387-22a05c9e6166.jpg?2219415037387239399" width="100%" /> <!--Red legging Plus--> <!-- 6 + -->
+<div class="w3-display-position" style="left: 27%; top: 12%; right: 10%; bottom: 15%;">
+<div class="dropdown"><img src="https://cdn.shopify.com/s/files/1/1970/7471/files/blue_plus.png?17232816934268540656" width="50" height="50" />
+<div class="dropdown-content w3-card-4" style="width: 250px;">
+<div class="w3-row">
+<div class="w3-half"><img src="//cdn.shopify.com/s/files/1/1970/7471/files/product-image-276349838_grande_9b0302d4-34c8-4b8e-b367-b684de4cf7cf_large.jpg?v=1507231451" alt="" width="158" height="132" /></div>
+<div class="w3-half">
+<p style="font-size: 12px; padding-left: 5px;">Compression Power leggings.</p>
+<div class="w3-display-container w3-padding">
+<p><button class="w3-right w3-button w3-red w3-tiny">BUY</button></p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<!--White Legging Plus--> <!-- 7 + -->
+<div class="w3-display-position" style="left: 68%; top: 10%; right: 20%; bottom: 15%;">
+<div class="dropdown"><img src="https://cdn.shopify.com/s/files/1/1970/7471/files/blue_plus.png?13562271075209030075" width="50" height="50" />
+<div class="dropdown-content w3-card-4" style="width: 250px;">
+<div class="w3-row">
+<div class="w3-half"><img src="//cdn.shopify.com/s/files/1/1970/7471/files/product-image-306369116_grande_878e647a-91f7-4c0b-b290-35477be1ebfa_large.jpg?v=1507231437" alt="" width="143" height="143" /></div>
+<div class="w3-half">
+<p style="font-size: 12px; padding-left: 5px;">Chic,with elegant mesh design fiting perfectly the feminine curve.</p>
+<div class="w3-display-container w3-padding">
+<p><button class="w3-right w3-button w3-red w3-tiny">BUY</button></p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<br />
+<p class="w3-text-black w3-hide-large"><b><i>THESE TECHNICAL LEGGINGS ARE PERFECT TO TAKE YOU FROM STUDIO TO STREET. IF YOU ARE A RUNNER THEY WILL CHANGE YOUR LIFE.</i></b></p>
+<a class="w3-text-black w3-hide-large" href="#" style="font-size: 15px;">Why she loves the leggings.</a> <br /> <br />
+<div class="w3-display-container w3-row-padding"><img src="https://cdn.shopify.com/s/files/1/1970/7471/files/women-capris-leggings-2_banner.jpg?17470390468230935948" width="100%" height="60%" />
+<div class="w3-display-middle">
+<h4 class="w3-text-white w3-hide-small"><b>THESE TECHNICAL LEGGINGS ARE PERFECT TO TAKE YOU FROM STUDIO TO STREET. IF YOU ARE A RUNNER THEY WILL CHANGE YOUR LIFE.</b></h4>
+<a class="w3-text-white w3-hide-small" href="#">Why she loves the leggings.</a></div>
+</div>
+<br /><br />
+<div class="w3-row-padding">
+<div class="w3-content w3-display-container">
+<div class="w3-display-container mySlides" style="display: block;">
+<div class="w3-row">
+<div class="w3-quarter"><img src="https://cdn.shopify.com/s/files/1/1970/7471/files/01-bumsculpting.gif?15458171718755883332" style="width: 100%;" /></div>
+<div class="w3-quarter"><img src="l2.jpg" style="width: 100%;" /></div>
+<div class="w3-quarter"><img src="https://cdn.shopify.com/s/files/1/1970/7471/files/01-bumsculpting.gif?15458171718755883332" style="width: 100%;" /></div>
+<div class="w3-quarter"><img src="l5.jpg" style="width: 100%;" /></div>
+</div>
+</div>
+<div class="w3-display-container mySlides" style="display: none;">
+<div class="w3-row">
+<div class="w3-quarter"><img src="l5.jpg" style="width: 100%;" /></div>
+<div class="w3-quarter"><img src="l6.jpg" style="width: 100%;" /></div>
+<div class="w3-quarter"><img src="l7.jpg" style="width: 100%;" /></div>
+<div class="w3-quarter"><img src="l8.jpg" style="width: 100%;" /></div>
+</div>
+</div>
+<button class="w3-button w3-display-left w3-black" onclick="plusDivs(-1)">❮</button> <button class="w3-button w3-display-right w3-black" onclick="plusDivs(1)">❯</button></div>
+<script>// <![CDATA[
+var slideIndex = 1; showDivs(slideIndex); function plusDivs(n) { showDivs(slideIndex += n); } function showDivs(n) { var i; var x = document.getElementsByClassName("mySlides"); if (n > x.length) {slideIndex = 1} if (n < 1) {slideIndex = x.length} for (i = 0; i < x.length; i++) { x[i].style.display = "none"; } x[slideIndex-1].style.display = "block"; }
+// ]]></script></div>
+<br /><br />
+<div class="w3-row-padding">
+<div class="w3-content">
+<center>
+<div class="mySlides1 w3-container w3-grey" style="display: none;">
+<h1><b>Feel Lifted</b></h1>
+<h1><i>I love the lightweight fabric, immediately feel ‘lifted’ and fabric keeps your cool – Diane London </i></h1>
+</div>
+<div class="mySlides1 w3-container w3-grey" style="display: none;">
+<h1><b>Superb</b></h1>
+<h1><i> The fit is superb and the material is so light… I think they actually help you to work out faster and harder! – Joanna California </i></h1>
+</div>
+<div class="mySlides1 w3-container w3-grey" style="display: none;">
+<h1><b>Very Impressed</b></h1>
+<h1><i>I am very impressed with the quality of the fabric and fit. They are a joy to wear either running, walking or in the gym! Nancy – New york </i></h1>
+</div>
+<div class="mySlides1 w3-container w3-grey" style="display: block;">
+<h1><b>Mental Boost</b></h1>
+<h1><i>These beautiful activewear give me the mental boost needed to push through a tough workout, I love them – Ema, Dallas</i></h1>
+</div>
+<div class="mySlides1 w3-container w3-grey" style="display: none;">
+<h1><b>Color my day</b></h1>
+<h1><i>Definitely feel fun and happy wearing these beautiful activewear - Claudia, London</i></h1>
+</div>
+</center>
+<script>// <![CDATA[
+var slideIndex = 0; carousel(); function carousel() { var i; var x = document.getElementsByClassName("mySlides1"); for (i = 0; i < x.length; i++) { x[i].style.display = "none"; } slideIndex++; if (slideIndex > x.length) { slideIndex = 1 } x[slideIndex-1].style.display = "block"; setTimeout(carousel, 5000); }
+// ]]></script></div>
+</div>
+<!-- END w3-content --></div>
+</div>
